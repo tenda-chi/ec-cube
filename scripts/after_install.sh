12f598add0ab9a53d7937eed59e953fb7f98a601
tenda-chi <chi@tenda.co.jp>
2023-12-18 21:23:28 +0900

    Update after_install.sh

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-echo "after_install.sh start..."
+LOG_FILE=/tmp/codedeploy.log
+echo "after_install.sh start..." | tee -a $LOG_FILE
+date | tee -a $LOG_FILE
 
 SOURCE_DIR=/var/codedeploy/ec-cube/
 TARGET_DIR=/home/ec-cube/
@@ -23,13 +25,13 @@ rsync -vaW --delete-after \
     --exclude "*.log" \
     --exclude "*.idea" \
     --exclude "*.settings" \
-    $SOURCE_DIR $TARGET_DIR
+    $SOURCE_DIR $TARGET_DIR 2>&1 | tee -a $LOG_FILE
 
 # Application ディレクトリに移動
 cd $TARGET_DIR
 
 # Composerを使用して依存関係をインストール
-composer install --no-interaction --prefer-dist --optimize-autoloader
+composer install --no-interaction --prefer-dist --optimize-autoloader | tee -a $LOG_FILE
 
 # マイグレーション
 # php artisan migrate --force
@@ -37,5 +39,5 @@ composer install --no-interaction --prefer-dist --optimize-autoloader
 # キャッシュのクリア
 # php artisan cache:clear
 
-echo "after_install.sh end."
+echo "after_install.sh end." | tee -a $LOG_FILE
 
