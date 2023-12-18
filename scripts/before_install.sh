8a163a3b9552d3bf633570ba71d5c51af1416c6a
tenda-chi <chi@tenda.co.jp>
2023-12-18 21:25:42 +0900

    Update before_install.sh

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-echo "before_install.sh start..."
+LOG_FILE=/tmp/codedeploy.log
+echo "before_install.sh start..." | tee -a $LOG_FILE
+date | tee -a $LOG_FILE
 
 # 既存のキャッシュや一時ファイルのクリーンアップ
 # rm -rf /var/www/html/test/bootstrap/cache/*.php
@@ -8,4 +10,4 @@ echo "before_install.sh start..."
 # 設定ファイルのバックアップ
 # cp /var/www/html/test/.env /var/www/html/test/.env.backup
 
-echo "before_install.sh end."
+echo "before_install.sh end." | tee -a $LOG_FILE
