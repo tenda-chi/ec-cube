b5b5d5416afa15b8acb3e91a7ca445ad0da424d8
tenda-chi <chi@tenda.co.jp>
2023-12-18 21:31:33 +0900

    Update application_start.sh

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 LOG_FILE=/tmp/codedeploy.log
-echo "application_install.sh start..." | tee -a $LOG_FILE
+echo "application_start.sh start..." | tee -a $LOG_FILE
 date | tee -a $LOG_FILE
 
 # Apacheサーバーの起動
 # sudo systemctl start httpd
 
-echo "application_install.sh end." | tee -a $LOG_FILE
+echo "application_start.sh end." | tee -a $LOG_FILE
