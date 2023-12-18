c02f722df6e13909ada37428e4592908bd93a4d2
tenda-chi <chi@tenda.co.jp>
2023-12-18 21:26:50 +0900

    Update validate_service.sh

--- scripts/validate_service.sh
+++ scripts/validate_service.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-echo "validate_service.sh start..."
+LOG_FILE=/tmp/codedeploy.log
+echo "validate_service.sh start..." | tee -a $LOG_FILE
+date | tee -a $LOG_FILE
 
 # ヘルスチェック系の処理
 
-echo "validate_service.sh end."
+echo "validate_service.sh end." | tee -a $LOG_FILE
